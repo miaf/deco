47226d74ae7eba2aaef51bd4e2b14feee9eb6966
Alex B. Miaffo Z <alex.miaffo@gmail.com>
2015-07-29 17:35:42 +0100

    email confirmation in place

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.signup.subject
+  #
+  def signup(user)
+    @user = user
+
+    mail to: @user.email
+  end
+end
